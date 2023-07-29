b8d92387eaaa6963cc631ed06f87eb3bd167d6eb
narutyo <narutyo@narutyo.work>
2023-07-29 20:40:17 +0900

    初期設定

---
+++ app/Events/MessageRecieved.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageRecieved implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('test');
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'data' => 'test',
+        ];
+    }
+}
+
